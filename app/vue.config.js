380fc7327c0a7f386422ccb6fc26e3815cb60e98
João Gabriel Messi Lutz <tibepc@gmail.com>
2022-08-23 12:31:38 -0300

    correção

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,8 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-<<<<<<< HEAD
-  //publicPath: '../'
-=======
->>>>>>> bb7198b9bd5084fd66adfceef8195caf518710a2
 })
